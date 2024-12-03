965a063478b5430f58a9f92287d9f979a7726896
Denny Rubino <rubidenny@gmail.com>
2024-12-03 11:51:12 +0100

    re-fix snack1

--- Snack-1/script.js
+++ Snack-1/script.js
@@ -1,9 +1,9 @@
 //L’utente inserisce due numeri in successione, con due prompt.
 //Il software stampa il maggiore
 
-const number1 = prompt("inserisci numero")
+const number1 = Number(prompt("inserisci numero"))
 
-const number2 = prompt("inserisci numero")
+const number2 = Number(prompt("inserisci numero"))
 
 if (number1 > number2) {alert(`Il numero maggiore è: ${number1}`);}
     
